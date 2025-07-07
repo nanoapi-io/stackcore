7f22656dede37149b928ef4845a4ba80a77da280
florianbgt <florian.bigot321@gmail.com>
2025-07-07 12:15:31 +0200

    add logs

--- packages/core/src/db/migrations/0002.migration.ts
+++ packages/core/src/db/migrations/0002.migration.ts
@@ -7,30 +7,44 @@ import type { dependencyManifestTypes } from "@stackcore/shared";
 
 // deno-lint-ignore no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
+  console.info("starting migration 0002....");
+
+  console.info("getting manifest records....");
   const manifestRecords = await db.selectFrom("manifest").selectAll()
     .execute();
 
+  console.info(`found ${manifestRecords.length} manifest records....`);
+
   for (const manifestRecord of manifestRecords) {
-    const manifestJson = await downloadJsonFromBucket(
-      manifestRecord.manifest,
-    ) as dependencyManifestTypes.DependencyManifest;
+    console.info(`processing manifest ${manifestRecord.manifest}....`);
 
-    for (
-      const fileManifestKey of Object.keys(manifestJson)
-    ) {
+    try {
+      const manifestJson = await downloadJsonFromBucket(
+        manifestRecord.manifest,
+      ) as dependencyManifestTypes.DependencyManifest;
+
       for (
-        const symbolManifestKey of Object.keys(
-          manifestJson[fileManifestKey].symbols,
-        )
+        const fileManifestKey of Object.keys(manifestJson)
       ) {
-        // set empty description to match new schema
-        manifestJson[fileManifestKey].symbols[symbolManifestKey].description =
-          "";
+        for (
+          const symbolManifestKey of Object.keys(
+            manifestJson[fileManifestKey].symbols,
+          )
+        ) {
+          // set empty description to match new schema
+          manifestJson[fileManifestKey].symbols[symbolManifestKey].description =
+            "";
+        }
       }
-    }
 
-    await uploadJsonToBucket(manifestJson, manifestRecord.manifest);
+      await uploadJsonToBucket(manifestJson, manifestRecord.manifest);
+    } catch (error) {
+      console.error(`error processing manifest ${manifestRecord.manifest}....`);
+      console.error(error);
+    }
   }
+
+  console.info("migration 0002 completed....");
 }
 
 // deno-lint-ignore no-explicit-any
